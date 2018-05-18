031fadd980aafc866adec81dddf8cc9baf0d4fa1
nelsonic <contact.nelsonic+github@gmail.com>
2018-05-18 20:16:20 +0100

    add initial files after running "mix new fields" generator command

---
+++ lib/fields.ex
@@ -0,0 +1,18 @@
+defmodule Fields do
+  @moduledoc """
+  Documentation for Fields.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Fields.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/fields_test.exs
@@ -0,0 +1,8 @@
+defmodule FieldsTest do
+  use ExUnit.Case
+  doctest Fields
+
+  test "greets the world" do
+    assert Fields.hello() == :world
+  end
+end
