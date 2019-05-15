5bd934479757588002efbb3c1dce422d38ada7f6
nelsonic <nelson+github@dwyl.io>
2019-05-15 14:43:47 +0100

    update mix.exs version to 0.1.5 for #33

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Fields.MixProject do
   def project do
     [
       app: :fields,
-      version: "0.1.3",
+      version: "0.1.5",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps()
