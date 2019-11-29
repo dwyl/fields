8c380309339128f7db8df9a9bcbe09e4ebbc9f01
nelsonic <nelson+github@dwyl.io>
2019-11-29 20:53:06 +0000

    version bump to 0.1.8 derp!

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Fields.MixProject do
     [
       app: :fields,
       description: "a collection of useful fields for building Phoenix apps faster!",
-      version: "0.0.1",
+      version: "0.1.8",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
