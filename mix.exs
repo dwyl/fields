c86704ba5678735198e5b4d2b77e646442ba77d8
nelsonic <nelson+github@dwyl.io>
2020-09-13 21:09:59 +0100

    maintenance update all dependencies #74

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Fields.MixProject do
     [
       app: :fields,
       description: "A collection of useful fields for building Phoenix apps faster!",
-      version: "2.6.0",
+      version: "2.7.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
@@ -34,17 +34,17 @@ defmodule Fields.MixProject do
       # password hashing
       {:argon2_elixir, "~> 2.3.0"},
       # ecto types
-      {:ecto, "~> 3.4.3"},
+      {:ecto, "~> 3.4.6"},
       # strip noise from html field
-      {:html_sanitize_ex, "~> 1.4"},
+      {:html_sanitize_ex, "~> 1.4.1"},
 
       # stream_data for property based testing
-      {:stream_data, "~> 0.4.3", only: :test},
+      {:stream_data, "~> 0.5.0", only: :test},
       # tracking test coverage
-      {:excoveralls, "~> 0.12.3", only: [:test, :dev]},
+      {:excoveralls, "~> 0.13.1", only: [:test, :dev]},
 
       # documentation
-      {:ex_doc, "~> 0.21.3", only: :dev},
+      {:ex_doc, "~> 0.22.5", only: :dev},
       {:inch_ex, only: :docs}
     ]
   end
