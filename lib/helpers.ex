ad19dd343ead9127408e83f33a02bb0e1a825861
Michael Bianco <iloveitaly@gmail.com>
2020-10-28 08:20:46 -0600

    Removing duplicate @doc

--- lib/helpers.ex
+++ lib/helpers.ex
@@ -11,11 +11,6 @@ defmodule Fields.Helpers do
     Argon2.Base.hash_password(to_string(value), Argon2.gen_salt(), [{:argon2_type, 2}])
   end
 
-  @doc """
-  Hash a string, or a value that implements the String.Chars protocol, using
-  sha256. sha256 is fast, but not as strong as Argon2,
-  so it is not recommended for hashing passwords.
-  """
   @spec hash(atom(), String.Chars.t()) :: String.t()
   def hash(:sha256, value) do
     :crypto.hash(:sha256, to_string(value) <> get_salt(to_string(value)))
