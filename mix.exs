854c8c5c5e1f818d3e061b4ce2850f0bd78e1cbb
Tom Haines <tom@tomhaines.xyz>
2021-08-03 21:39:44 +0100

    Add test supports to compiler path

--- mix.exs
+++ mix.exs
@@ -10,6 +10,7 @@ defmodule Fields.MixProject do
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       package: package(),
+      elixirc_paths: elixirc_paths(Mix.env()),
 
       # coverage
       test_coverage: [tool: ExCoveralls],
@@ -27,6 +28,9 @@ defmodule Fields.MixProject do
       extra_applications: [:logger]
     ]
   end
+
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
