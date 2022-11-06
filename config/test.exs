627701cd01018a682221de10331cc27d598fd605
removedporn <86824510+removedporn@users.noreply.github.com>
2022-11-06 15:44:37 +0800

    Delete config directory

--- config/test.exs
+++
@@ -1,5 +0,0 @@
-import Mix.Config
-
-config :fields,
-  encryption_keys: "nMdayQpR0aoasLaq1g94FLba+A+wB44JLko47sVQXMg=,L+ZVX8iheoqgqb22mUpATmMDsvVGtafoAeb0KN5uWf0=",
-  secret_key_base: "GLH2S6EU0eZt+GSEmb5wEtonWO847hsQ9fck0APr4VgXEdp9EKfni2WO61z0DMOF"
